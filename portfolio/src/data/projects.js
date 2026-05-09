1a5d59408fd6456a1ab3c5f66bc760d00e171fe6
Ayoub <elhoudaiguiayoub2@gmail.com>
2026-05-09 01:59:28 +0100

    Update portfolio

--- portfolio/src/data/projects.js
+++ portfolio/src/data/projects.js
@@ -1,13 +1,13 @@
 export const projects = [
   {
     id: 1,
-    title: "Task Manager",
+    title: "ecommerce Website",
     category: "React",
     description:
-      "A modern task manager with localStorage, filtering, completed states, and clean UI.",
-    image: "/images/todo.png",
-    github: "https://github.com/elhoudaiguiayoub/todoapp",
-    demo: "https://todoapp-ruby-chi.vercel.app/",
+      "A premium-style ecommerce website built with React, React Router, reusable components, modal project cards, and responsive design.",
+    image: "/images/ecommerce.png",
+    github: "https://github.com/elhoudaiguiayoub/ehyoustreetwear",
+    demo: "https://ehyoureetwear.vercel.app/",
   },
 
   {
@@ -31,4 +31,15 @@ export const projects = [
     github: "https://github.com/elhoudaiguiayoub/portfolioayoub",
     demo: "https://portfolioayoub-keis-ebon.vercel.app/",
   },
+
+  {
+    id: 4,
+    title: "Task Manager",
+    category: "React",
+    description:
+      "A modern task manager with localStorage, filtering, completed states, and clean UI.",
+    image: "/images/todo.png",
+    github: "https://github.com/elhoudaiguiayoub/todoapp",
+    demo: "https://todoapp-ruby-chi.vercel.app/",
+  }
 ];
